a62fbf7e206c649a6c7475945cd2f7c498319268
Santi <SantiAlexanderYang@Gmail.com>
2018-07-11 09:39:55 +0800

    0711

--- src/platforms/web/web.entry.js
+++ src/platforms/web/web.entry.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import weex from 'weex-vue-render';
 weex.init(Vue);
-import Home from '../../pages/home/index.vue';
+const Home = require('../../pages/home/index.vue');
 
 new Vue(Vue.util.extend({
   el:'#root'
